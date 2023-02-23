83c38ad95a4af3a6ed79166aecb3bcb5ba545620
duyguseelin <duyguselincakir@ogrenci.beykoz.edu.tr>
2023-02-23 18:18:45 +0300

    nesne yönelim örneği

---
+++ nesne yönelim .cpp
@@ -0,0 +1,116 @@
+//
+//  main.cpp
+//  nesne yönelimi
+//
+//  Created by duygu selin çakır on 9.02.2023.
+//
+
+#include <iostream>
+using namespace std;
+
+
+/*
+// piramid
+    int main() {
+    int i,j;
+    for(i=5;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+            cout<<i;
+        }
+    }
+    */
+    
+
+    
+/*
+// kullanıcıdan alınan bir sayi 3,5 ise basarılı-1 6-9 arasında ve çift şse başarılı-2 diğerlerinden ise başarısız yazdır
+    int main() {
+    int sayi=0;
+    cout<<"bir sayi giriniz(1-10)";
+    cin>>sayi;
+    if(sayi==3 || sayi==5)
+    {
+        cout<<"basarili-1";
+    }
+    else if(6<sayi<9 && sayi%2==0)
+    {
+        cout<<"basarisiz-2";
+    }
+    else
+    {
+        cout<<"basarisiz";
+    }
+    */
+    
+   
+/*
+//iki sayıyı çarpan fonksiyon
+int carpim (int sayi1, int sayi2)
+{
+    return sayi1*sayi2;
+}
+int main ()
+{
+    int sayi1, sayi2;
+    cout<<"sayi1'i giriniz";
+    cin>>sayi1;
+    cout<<"sayi'2yi giriniz";
+    cin>>sayi2;
+    cout<<"sonuc"<<carpim(sayi1,sayi2);
+
+    */
+   
+
+
+/*
+inline int dikdortgen (int uzun , int kisa)
+{
+    return 2*(uzun+kisa) ;
+}
+int main()
+{
+    int uzun ,kisa;
+    cout<<"uzun kenari giriniz";
+    cin>>uzun;
+    cout<<"kisa kenari giriniz";
+    cin>>kisa;
+    cout<<"dikdortgenin cevresi"<<dikdortgen(uzun,kisa);
+  */
+
+
+
+/*
+int alan( int kisa=1, int uzun=1);
+int main()
+{
+    cout<<"kenar ölçüleri 5,1 olanin alani"<<alan(5);
+}
+int alan(int kisa, int uzun)
+{
+    return kisa*uzun;
+}
+*/
+
+
+
+/*
+//lokal global
+int numara=6;
+int main()
+{
+    double munara=8.5;
+    cout<<"global numara:"<< :: numara <<"\nlokal numara:"<< numara;
+*/
+
+
+
+
+
+
+
+
+}
+
+
