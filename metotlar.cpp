37b9b4c71cc39f5e78be91f18dbb95fea034d13c
duyguseelin <duyguselincakir@ogrenci.beykoz.edu.tr>
2023-02-24 18:36:47 +0300

    .

---
+++ metotlar.cpp
@@ -0,0 +1,135 @@
+#include <stdio.h>
+/*
+//2 tam sayının toplamını alıp 5e bölen
+int hesap(int sayi1, int sayi2)
+{
+    int sonuc;
+    sonuc=(sayi1+sayi2)/5;
+    return sonuc;
+    }
+int main()
+{
+    printf("%d",hesap(100,200));
+*/
+
+
+
+/*
+//girilen iki sayiyi toplayan
+void topla()
+{
+int sayi1, sayi2;
+printf("degerleri giriniz : ");
+scanf("%d %d" , &sayi1 , &sayi2);
+printf("toplam: %d",sayi1+sayi2);
+}
+int main()
+{
+topla();
+*/
+
+
+/*
+//girilen iki sayıyı çarpan
+void carpim()
+{
+    int sayi1, sayi2;
+    printf("degerleri giriniz");
+    scanf("%d %d", &sayi1 , &sayi2);
+    carpim = sayi1*sayi2;
+    printf("carpim= %d",carpim);
+    }
+int main()
+    {
+        carpim();
+ */
+  
+
+/*
+//girilen sayı tek mi çift mi
+    void tek_cift(int sayi)
+{
+        printf("sayi giriniz");
+        scanf("%d", & sayi);
+        if(sayi%2==0)
+        {
+            printf("sayi ciftir");
+        }
+        else{
+            printf("sayi tektir");
+        }
+        }
+        
+    int main()
+{
+        int sayi;
+        tek_cift(sayi);
+ */
+
+/*
+//dikdörtgen cevre alan heabı
+int cevre(int kisa , int uzun)
+{
+    return kisa*2+uzun*2;
+    }
+int alan(int kisa, int uzun)
+{
+    return kisa*uzun;
+    }
+  int main()
+{
+      int kisa, uzun;
+      printf("kenar uzunluklarini giriniz:");
+      scanf("%d %d",&uzun ,&kisa);
+      printf("alan: %d\n",alan(kisa,uzun));
+      printf("cevre: %d\n",cevre(kisa,uzun));
+*/
+
+/*
+//faktoriyel hesabı
+void sonuc(int sayi, int sayac, int faktoriyel)
+{
+    faktoriyel=1;
+    printf("bir sayi giriniz: ");
+    scanf("%d",&sayi);
+    for(sayac=1;sayac<=sayi;sayac++)
+    {
+        faktoriyel = faktoriyel * sayac;
+    }
+    printf("faktoriyel: %d",faktoriyel);
+    }
+int main()
+{
+    int faktoriyel;
+    int sayi;
+    int sayac;
+    sonuc(faktoriyel,sayi,sayac);
+*/
+
+/*
+//dersten geçtin/kaldın
+void not(float sonuc , int vize , int final )
+{
+    vize=50;
+    final=45;
+    sonuc=(vize*0.4)+(final*0.6);
+    if(sonuc>50)
+    {
+        printf("gecdiniz");
+    }
+    else
+    {
+        printf("kaldiniz");
+    }
+    }
+int main()
+{
+    float sonuc;
+    int vize;
+    int final;
+    not(sonuc,vize,final);
+ */
+
+
+
+
